1982cdfa344c3555c10f5568228e755e2aa106a3
Jung kyu Lee <48317269+ljk9410@users.noreply.github.com>
2020-09-12 16:59:20 +0900

    20200912

---
+++ json.js
@@ -0,0 +1,39 @@
+//JSON
+//JavaScript Object Notation
+
+//1. Object to JSON
+//stringify(obj)
+let json = JSON.stringify(true);
+console.log(json);
+
+json = JSON.stringify(['apple', 'banana']);
+console.log(json);
+
+const rabbit = {
+    name: 'tori',
+    color: 'white',
+    size: null,
+    birthDate: new Date(),
+    jump: () => console.log(`${name} can jump!`)
+};
+
+json = JSON.stringify(rabbit, (key, value) => {
+    console.log(`key: ${key}, value: ${value}`);
+    return key === 'name' ? 'ellie' : value;
+});
+console.log(json);
+
+//2. JSON to Object
+//parse(json)
+console.clear();
+json = JSON.stringify(rabbit);
+const obj = JSON.parse(json, (key, value) => {
+    console.log(`key: ${key}, value: ${value}`);
+    return key === 'birthDate' ? new Date(value) : value;
+});
+console.log(obj);
+rabbit.jump();
+//obj.jump() => json은 함수를 변환하지 못하기 때문에 오류
+
+console.log(rabbit.birthDate.getDate());
+console.log(obj.birthDate);
